f4783d4f28e9371857482253be8552f46be032f8
denes-attila <150531154+denes-attila@users.noreply.github.com>
2026-03-18 18:11:58 +0100

    Refactor: extract HTML routes into Blueprint

--- app.py
+++ app.py
@@ -2,99 +2,14 @@ from flask import Flask, render_template, jsonify, request
 from database import get_db
 from dotenv import load_dotenv
 from tanulok_api import tanulok_api
+from tanulok_html import tanulok_html
 
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(tanulok_api)
-
-@app.route("/")
-def index():
-    meresek = ["suly", "magassag", "testzsir", "tavolugrás", "ingafutas",
-           "fekvotamasz", "hajlekonysag", "szoritoeró", "torzsemeles"]
-    return render_template("index.html", meresek = meresek)
-
-
-
-
-@app.route("/tanulok")
-def tanulok():
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT DISTINCT nev
-        FROM meresek
-    """)
-
-    sorok = cursor.fetchall()
-    conn.close()
-
-    nevek = [sor[0] for sor in sorok]
-
-    return render_template("tanulok.html", nevek = nevek)
-
-
-@app.route("/tanulo/<nev>")
-def tanulo(nev):
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT * 
-        FROM meresek
-        WHERE nev = ?
-        
-    """, (nev,))
-
-    tanulo_adatok = [dict(sor) for sor in cursor.fetchall()]
-    elso = [ sor for sor in tanulo_adatok if sor["datum"] == '2025-03-01']
-    masodik = [ sor for sor in tanulo_adatok if sor["datum"] == '2026-03-01']
-    conn.close()
-    return render_template("tanulo.html", elso = elso, masodik = masodik) 
-
-@app.route("/atlagok")
-def atlagok():
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute("""
-    SELECT nem, AVG(fekvotamasz)
-    FROM meresek
-    GROUP BY nem
-    """)
-    fekvotamasz_atlagok  = [dict(sor) for sor in cursor.fetchall()]
-    conn.close()
-
-    return render_template("atlagok.html", fekvotamasz_atlagok = fekvotamasz_atlagok)
-
-@app.route("/atlagok/<meres>")
-
-
-def osszes_atlag(meres):
-    conn = get_db()
-    cursor = conn.cursor()
-
-
-    meresek = ["suly", "magassag", "testzsir", "tavolugrás", "ingafutas",
-           "fekvotamasz", "hajlekonysag", "szoritoeró", "torzsemeles"]
-    
-
-    if meres not in meresek:
-        return "Érvénytelen mérés!", 400
-    query = (f"""
-    SELECT nem, AVG({meres}) as atlag
-    FROM meresek
-    WHERE datum = '2025-03-01'
-    GROUP BY nem
-    """)
-    print(query)
-
-
-    cursor.execute(query)
-
-    meres_atlagok = [dict(sor) for sor in cursor.fetchall()]
-    conn.close()
-    return render_template("meres.html", meres_atlagok = meres_atlagok, meresek = meresek)
-
+app.register_blueprint(tanulok_html)
 
 
 if __name__ == "__main__":
---
+++ tanulok_html.py
@@ -0,0 +1,90 @@
+from flask import Blueprint, render_template
+from database import get_db
+
+tanulok_html = Blueprint('tanulok_html', __name__)
+
+@tanulok_html.route("/")
+def index():
+    meresek = ["suly", "magassag", "testzsir", "tavolugrás", "ingafutas",
+           "fekvotamasz", "hajlekonysag", "szoritoeró", "torzsemeles"]
+    return render_template("index.html", meresek = meresek)
+
+
+
+
+@tanulok_html.route("/tanulok")
+def tanulok():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT DISTINCT nev
+        FROM meresek
+    """)
+
+    sorok = cursor.fetchall()
+    conn.close()
+
+    nevek = [sor[0] for sor in sorok]
+
+    return render_template("tanulok.html", nevek = nevek)
+
+
+@tanulok_html.route("/tanulo/<nev>")
+def tanulo(nev):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT * 
+        FROM meresek
+        WHERE nev = ?
+        
+    """, (nev,))
+
+    tanulo_adatok = [dict(sor) for sor in cursor.fetchall()]
+    elso = [ sor for sor in tanulo_adatok if sor["datum"] == '2025-03-01']
+    masodik = [ sor for sor in tanulo_adatok if sor["datum"] == '2026-03-01']
+    conn.close()
+    return render_template("tanulo.html", elso = elso, masodik = masodik) 
+
+@tanulok_html.route("/atlagok")
+def atlagok():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+    SELECT nem, AVG(fekvotamasz)
+    FROM meresek
+    GROUP BY nem
+    """)
+    fekvotamasz_atlagok  = [dict(sor) for sor in cursor.fetchall()]
+    conn.close()
+
+    return render_template("atlagok.html", fekvotamasz_atlagok = fekvotamasz_atlagok)
+
+@tanulok_html.route("/atlagok/<meres>")
+
+
+def osszes_atlag(meres):
+    conn = get_db()
+    cursor = conn.cursor()
+
+
+    meresek = ["suly", "magassag", "testzsir", "tavolugrás", "ingafutas",
+           "fekvotamasz", "hajlekonysag", "szoritoeró", "torzsemeles"]
+    
+
+    if meres not in meresek:
+        return "Érvénytelen mérés!", 400
+    query = (f"""
+    SELECT nem, AVG({meres}) as atlag
+    FROM meresek
+    WHERE datum = '2025-03-01'
+    GROUP BY nem
+    """)
+    print(query)
+
+
+    cursor.execute(query)
+
+    meres_atlagok = [dict(sor) for sor in cursor.fetchall()]
+    conn.close()
+    return render_template("meres.html", meres_atlagok = meres_atlagok, meresek = meresek)
