0931ca818d99832cd78b897958008c9b54ebed17
denes-attila <150531154+denes-attila@users.noreply.github.com>
2026-03-26 22:01:55 +0100

    Add auth blueprint with login route and User model

---
+++ auth.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, render_template, redirect, url_for
+from database import get_db
+from flask_login import login_user
+from user import User
+auth = Blueprint('auth', __name__)
+
+@auth.route("/auth", methods = ['GET', 'POST'])
+def auth():
+    if request.method == 'GET':
+        return render_template('logint.html')
+    else:
+        conn = get_db()
+        cursor = conn.cursor()
+        adat = request.form
+        name = adat['username']
+        passwd = adat['password']
+        cursor.execute(
+            """
+            SELECT  * FROM users WHERE username = ?
+
+        """, (name,)
+        )
+        db_data = cursor.fetchone()
+        if db_data is None:
+            return render_template('login.html', error='Hibás felhasználónév vagy jelszó')
+        if passwd == db_data["password"]:
+            user = User(db_data['id'], db_data['username'], db_data['password'])
+            login_user(user)
+            return redirect(url_for('tanulok_html.index'))
+        else:
+            return render_template('login.html', error='Hibás felhasználónév vagy jelszó')  
